833514903b0cc5525493e0b1037171d04903ef2e
guoxh <guoxuehui@51jiabo.com>
2019-11-21 15:01:51 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.scss';
 
 function App() {
@@ -7,16 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.guoxh.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          welcome to my person station
         </a>
       </header>
     </div>
